6165a49872a4a88705b6be4ea397a8d6e38463f7
Joshua Pham <joshuatp709@gmail.com>
2026-08-27 01:22:09 -0700

    Initial Commit

---
+++ PopNotch/PopNotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopNotchApp.swift
+//  PopNotch
+//
+//  Created by Joshua Pham on 8/27/26.
+//
+
+import SwiftUI
+
+@main
+struct PopNotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
